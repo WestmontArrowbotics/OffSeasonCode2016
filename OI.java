92a87294a2e8e06b8e4d1ee6bde03cd23e5cccba
SimiusFerrum <4jlevy89@gmail.com>
2016-09-27 00:03:53 -0700

    Update OI.java

--- OI.java
+++ OI.java
@@ -1,14 +1,30 @@
 package org.usfirst.frc3482.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
+import edu.wpi.first.wpilibj.buttons.*;
+import org.usfirst.frc3482.robot.commands.*;
+import org.usfirst.frc3482.robot.subsystems.*;
 public class OI {
 
 	public Joystick xboxController;
+	public JoystickButton startFeedButton;
+	public JoystickButton reverseFeedButton;
+	public Joystick joystick;
 	public OI() {
 		xboxController = new Joystick(0);
+		joystick = new Joystick(1);
+		
+		startFeedButton = new JoystickButton(joystick, 1);
+		startFeedButton.whileHeld(new FeedShooter(true));
+		
+		reverseFeedButton = new JoystickButton(joystick, 2);
+		reverseFeedButton.whileHeld(new FeedShooter(false));
 	}
 	public Joystick getxboxController() {
 		return xboxController;
+	}
+	
+	public Joystick getJoystick() {
+		return joystick;
 	}
 }
