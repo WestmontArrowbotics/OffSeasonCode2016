bb6e4d4ea912ec42a2dd3e32d3a5d56de0668c9e
SimiusFerrum <4jlevy89@gmail.com>
2016-10-24 11:55:45 -0700

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -1,75 +1,77 @@
-package org.usfirst.frc3482.robot;
+
+package org.usfirst.frc.team3482.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc3482.robot.subsystems.*;
-import org.usfirst.frc3482.robot.commands.*;
-
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
 public class Robot extends IterativeRobot {
-		
-	Command teleopCommand;
-	Command autonomousCommand;
-	//FUCK LINUX
-	//AYYYYYYYYYYYYLMAO
-	//#STOPTHEDAB
-	public static OI oi;
-	public static Chassis chassis;
-	public static Intake intake;
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
 	
-	public static SendableChooser autoChooser;
-		
-	public void robotInit() {
-		RobotMap.init();
-		chassis = new Chassis();
-		intake = new Intake();
-		
-		oi = new OI();
-	
-		//autoChooser = new SendableChooser();
-		//autonomousCommand = new AutonomousCommand;
-		teleopCommand = new Drive();
-	}
-	
-	public void disabledInit() {
-		
-	}
-	
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-	
-	public void autonomousInit() {
-		System.out.println("Autonomous Initiated");
-		//autonomousCommand = (Command) autoChooser.getSelected();
-		if (autonomousCommand != null) autonomousCommand.start();
-	}
-	
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-		
-	}
-	
-	public void teleopInit() {
-		if (autonomousCommand != null) autonomousCommand.cancel();
-	}
-	
-	public void operatorControl() {
-		while(isOperatorControl() && isEnabled()) {
-			//Robot.chassis.drive(Robot.oi.getxboxController());
-			//Timer.delay(.05);
-		}
-	}
-	
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-		Robot.chassis.drive(Robot.oi.getxboxController());
-	}
-	
-	public void testPeriodic() {
-		
-	}
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+    	autoSelected = (String) chooser.getSelected();
+//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here   
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+            break;
+    	}
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
 }
