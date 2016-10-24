320178f2c77c830fdf54713a9bdf99ae5cf4037c
SimiusFerrum <4jlevy89@gmail.com>
2016-10-24 11:54:17 -0700

    Update OI.java

--- OI.java
+++ OI.java
@@ -4,12 +4,20 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.*;
 import org.usfirst.frc3482.robot.commands.*;
 import org.usfirst.frc3482.robot.subsystems.*;
+import edu.wpi.first.wpilibj.DigitalInput;
 public class OI {
 
 	public Joystick xboxController;
 	public JoystickButton startFeedButton;
 	public JoystickButton reverseFeedButton;
 	public Joystick joystick;
+	public JoystickButton spinIntakeButton;
+	public JoystickButton spinOuttakeButton;
+	public JoystickButton startShooterButton;
+	public JoystickButton raiseIntakeButton;
+	public JoystickButton lowerIntakeButton;
+	public JoystickButton getPositionButton;
+	
 	public OI() {
 		xboxController = new Joystick(0);
 		joystick = new Joystick(1);
@@ -19,6 +27,26 @@ public class OI {
 		
 		reverseFeedButton = new JoystickButton(joystick, 2);
 		reverseFeedButton.whileHeld(new FeedShooter(false));
+		
+		spinOuttakeButton = new JoystickButton(joystick, 3);
+		spinOuttakeButton.whenPressed(new SpinIntake(true));
+		
+		spinIntakeButton = new JoystickButton(joystick, 4);
+		spinIntakeButton.whenPressed(new SpinIntake(false));
+		
+		startShooterButton = new JoystickButton(joystick, 5);
+		startShooterButton.whileHeld(new Shoot());
+		
+		raiseIntakeButton = new JoystickButton(joystick, 6);
+		raiseIntakeButton.whileHeld(new MoveIntake(true));
+		
+		lowerIntakeButton = new JoystickButton(joystick, 7);
+		lowerIntakeButton.whileHeld(new MoveIntake(false));
+		
+		getPositionButton = new JoystickButton(joystick, 10);
+		getPositionButton.whenPressed(new GetPosition());
+		
+		
 	}
 	public Joystick getxboxController() {
 		return xboxController;
