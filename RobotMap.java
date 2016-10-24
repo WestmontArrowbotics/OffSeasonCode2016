a5bdc25da3f6f7de68db3837f57b96b7faa21475
SimiusFerrum <4jlevy89@gmail.com>
2016-10-24 11:58:25 -0700

    Update RobotMap.java

--- RobotMap.java
+++ RobotMap.java
@@ -5,6 +5,7 @@ import org.usfirst.frc3482.robot.subsystems.*;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class RobotMap {
 	public static CANTalon frontLeft;
@@ -13,6 +14,11 @@ public class RobotMap {
 	public static CANTalon backRight;
 	public static RobotDrive driveRobot;
 	public static Relay feedShooter;
+	public static CANTalon intakeBall;
+	public static DigitalInput limitSwitch;
+	public static CANTalon shootBall;
+	public static CANTalon raiseIntake;
+	public static CANTalon intake;
 	
 	public static void init() {
 		
@@ -23,6 +29,13 @@ public class RobotMap {
 		driveRobot = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
 		
 		feedShooter = new Relay(0);
+		
+		intakeBall = new CANTalon(7);
+		raiseIntake = new CANTalon(5);
+		
+		limitSwitch = new DigitalInput(2);
+		
+		shootBall = new CANTalon(4);
 		
 	}
 }
